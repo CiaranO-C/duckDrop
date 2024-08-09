4ced56428e081735771e5dbc342846ffe9a6f8a0
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-09 10:21:10 +0100

    create routes for sign up and login

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
-const indexController = require('../controllers/indexController')
-
+const indexController = require('../controllers/indexController');
+const passport = require('../config/passport');
 const indexRouter = Router();
 
 indexRouter.get('/', indexController.getHome);
+indexRouter.get('/sign-up', indexController.getSignUp);
+indexRouter.get('/login', indexController.getLogin);
+
+indexRouter.post('/sign-up', indexController.postSignUp);
+indexRouter.post('/login', passport.authenticate('local', { successRedirect: '/user', failureRedirect: '/login'}));
 
 module.exports = indexRouter;
