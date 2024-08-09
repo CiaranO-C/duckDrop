6ffebd9b7d87f0ac050614bd03aa8e11b0e378de
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-09 10:22:34 +0100

    create default folder router, logout router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,62 @@
-const { Router } = require('express');
-const { isUser } = require('../controllers/authenticate');
+const { Router } = require("express");
+const { isUser } = require("../controllers/authenticate");
 //const userController = require('../controllers/userController');
+const multer = require("multer");
+const { prisma } = require("../config/prisma");
+const upload = multer({ dest: "multer-uploads/" });
 
 const userRouter = Router();
 
 userRouter.use(isUser);
 
-userRouter.get('/', (req, res)=> {
-    res.send('you are authentico!')
-} );
+userRouter.get("/", async (req, res) => {
+    const rootFolder = await prisma.folder.findFirst({
+        where: {
+          ownerId: req.user.id,
+          isRoot: true,
+        },
+      });
+      res.redirect(`/user/folder/${rootFolder.id}`);
+});
 
+userRouter.get("/folder/:id", async (req, res, next) => {
+  const folder = await prisma.folder.findUnique({
+    where: { id: Number(req.params.id) },
+  });
+  const openModal = req.query.create === "true";
+  res.render("dashboard", {
+    currentFolder: folder,
+    modal: openModal,
+  });
+});
 
+userRouter.get("/logout", (req, res, next) => {
+  req.logOut((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+
+userRouter.get("/upload", (req, res, next) => {
+  res.render("upload-form");
+});
+
+userRouter.post("/upload", upload.single("userfile"), (req, res, next) => {
+  console.log(req.file);
+  res.render("upload-form");
+});
+userRouter.get("/:id/new-folder");
+userRouter.post("/:id/new-folder", async (req, res, next) => {
+  const createFolder = await prisma.folder.create({
+    data: {
+      ownerId: req.user.id,
+      title: req.body.title,
+      parentId: req.params.id,
+    },
+  });
+  res.redirect("/drive");
+});
 
 module.exports = userRouter;
