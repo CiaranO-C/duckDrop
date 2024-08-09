a8d9d7d3119179f91d7fecd3bfbcb9ca779c77ed
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-08-09 10:17:55 +0100

    formatting

--- controllers/authenticate.js
+++ controllers/authenticate.js
@@ -1,9 +1,17 @@
-function isUser(req, res, next){
-if(req.isAuthenticated()){
-    next()
-} else {
-    res.send('you aint a user!')
+function isUser(req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.send("you aint a user!");
+  }
 }
+
+function isAdmin(req, res, next) {
+  if (req.isAuthenticated() && req.user.role === "ADMIN") {
+    next();
+  } else {
+    res.send("you ain't an admin suckka!");
+  }
 }
 
 module.exports = { isUser };
